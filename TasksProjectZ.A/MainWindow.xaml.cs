692cb5ceb8e92b68b7366bb2b1c49e04d5414f83
LetY0uDown <maks.manin.04@gmail.com>
2024-02-16 11:17:50 +1000

    jftj

--- TasksProjectZ.A/MainWindow.xaml.cs
+++ TasksProjectZ.A/MainWindow.xaml.cs
@@ -27,18 +27,31 @@ namespace TasksProjectZ.A
             InitializeComponent();
 
             NewTask newTask = new NewTask();
-            var f = new FileInfo("tasks.json");
-            if (f.Exists && f.Length > 0)
-            {
-                using (FileStream fs = new FileStream("tasks.json", FileMode.OpenOrCreate))
-                {
-                    newTask.Task = JsonSerializer.Deserialize<Task>(fs);
-                }
-            }
-            else
-                newTask.Task = new Task();
+
+            //var f = new FileInfo("tasks.json");
+            //if (f.Exists && f.Length > 0)
+            //{
+            //    using (FileStream fs = new FileStream("tasks.json", FileMode.OpenOrCreate))
+            //    {
+            //        newTask.Task = JsonSerializer.Deserialize<Task>(fs);
+            //    }
+            //}
+            //else
+            //    newTask.Task = new Task();
 
 
+        }
+
+        private void NewTask_Click(object sender, RoutedEventArgs e)
+        {
+            NewTask task = new NewTask();
+            task.Show();
+        }
+
+        private void TaskHistory_Click(object sender, RoutedEventArgs e)
+        {
+            TaskHistory history = new TaskHistory();
+            history.Show();
         }
     }
 }
